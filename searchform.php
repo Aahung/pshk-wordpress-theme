b2efb9a5d141bd5676f4443923fe65d48f8be33c
aaHung <Landxh@gmail.com>
2016-03-12 00:21:58 +0800

    feat: Change Search form UI

--- searchform.php
+++ searchform.php
@@ -8,14 +8,13 @@
 
 <form role="search" method="get" class="widget callout" action="<?php echo home_url( '/' ); ?>">
     <div class="row">
-        <div class="large-8 columns prefix">
+        <div class="large-12 columns">
             <input type="search"
-                style="margin: 0 0 0rem;"
                 placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
                 value="<?php echo get_search_query() ?>" name="s"
                 title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
         </div>
-        <div class="large-4 columns postfix">
+        <div class="large-12 columns">
             <input style="margin: 0 0 0rem;" type="submit" class="button" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
         </div>
     </div>
